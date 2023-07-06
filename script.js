53c015cd427c25f21f79967469506f56b8265e0d
dhruvk-081 <115366526+dhruvk-081@users.noreply.github.com>
2023-07-06 08:45:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+let input = document.querySelector("#newTodoInput");
+let list = document.querySelector("#todoList");
+let addTodoBtn = document.querySelector("#addTodoBtn");
 
+addTodoBtn.addEventListener("click", addTodo);
+function addTodo(){
+    if(input.value !== ''){
+        const li = document.createElement("li");
+        li.innerText = input.value;
+        list.appendChild(li);
+        input.value = "";
+    }   
+}
